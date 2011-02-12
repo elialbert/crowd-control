ad90a44b41f1c95e7d72cde2e1049f22b2dcb4a9
elialbert <elialbert@gmail.com>
2011-02-12 17:23:49 -0600

    up and running! implemented threading, radius a bit better, client updates on geo loop

--- src/com/test/cc1/crowdcontrol.java
+++ src/com/test/cc1/crowdcontrol.java
@@ -8,6 +8,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.location.Location;
 import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Gravity;
@@ -16,7 +17,6 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.Window;
 import android.view.View.OnKeyListener;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -38,8 +38,10 @@ public class crowdcontrol extends Activity {
 	public int menuChoice;
 	public Location oldloc = new Location("init");
 	public Location curloc = new Location("cur");
+	public Location sendloc = new Location("send");
 	public ListView lv1;
 	public EditText ed;
+	public String tosend;
 	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -121,11 +123,17 @@ public class crowdcontrol extends Activity {
 		}
 		
 		if (menuChoice == R.id.radius) {
-	    	if (input.matches("[/d+]")) {
+	    	//if (input.matches("[/d+]")) {
+	    	try {
 	    		Radius = Long.valueOf(input);
-	    		setTitle(Username + ", Radius set to " + Radius);
+	    		setTitle(Username + ", radius set to " + Radius);
 	    		return;
 	    	}
+	    	catch (Exception e) {
+	    		Radius = 50;
+	    		setTitle(Username + ", radius error, radius set to 50");
+	    	}
+	    	//}
 	    	return;
 		}
 	}
@@ -146,10 +154,12 @@ public class crowdcontrol extends Activity {
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0,
 				0, new GeoLoc(this));
+		sendloc = null;
 		oldloc.setLatitude(71.0);
 		oldloc.setLongitude(-122.0);
 		curloc.setLatitude(71.0);
 		curloc.setLongitude(-122.0);
+		tosend = "";
 
         lv1.setAdapter(new ArrayAdapter<String>(this, R.layout.list_item, ENTRIES));
 
@@ -173,8 +183,11 @@ public class crowdcontrol extends Activity {
                 if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
                     (keyCode == KeyEvent.KEYCODE_ENTER)) {
                   // Perform action on key press
-                  update(String.valueOf(ed.getText()), null);
-                  return true;
+                  //update(String.valueOf(ed.getText()), null);
+                	sendloc = curloc; //make sure we send the current location over as well
+                	tosend = String.valueOf(ed.getText());
+            		new serverTask().execute(tosend);
+                	return true;
                 }
                 return false;
             }
@@ -182,27 +195,58 @@ public class crowdcontrol extends Activity {
         });
     }
     
+    
+    public class serverTask extends AsyncTask<String, Void, String> {
+    	
+    	//display the user's message
+    	@Override
+    	public void onPreExecute() {
+    		if ((tosend != null) && !tosend.matches("^\\s*$")) {
+    			ENTRIES.add(Username + ": " + tosend); //put the user's message on their own screen first, for a quicker feel
+    			tosend = "";
+    			ed.setText("");
+    			lv1.setAdapter(new ArrayAdapter<String>(crowdcontrol.this, R.layout.list_item, ENTRIES)); //display the new list item
+    		}
+    	}
+    	
+    	//update the server and get back any waiting messages
+        protected String doInBackground(String... inparams) {
+            return update(inparams[0], sendloc);
+        }
+        
+        //send any result to be printed
+        protected void onPostExecute(String result) {
+        	Log.w("SERVERTASK", result);
+        	Log.w("post", "in thread?");
+            crowdcontrol.this.response2list(result);
+        }
+    }
+    
+    
+    
     //this called by GeoLoc, updates location and sends to server if loc has moved a bunch
     public void updateLoc(Location loc) {
     	if (oldloc.distanceTo(loc) < 10) { //see if we've moved 10 meters
     		curloc = loc; //update the curloc, but still compare to the oldloc
-    		return;
+    		sendloc = null;
     	}
     	else {
-    		update("", loc);
+    		curloc = loc;
+    		sendloc = curloc;
     	}
+    	new serverTask().execute(""); //hit the server using the gps loop as our driver loop
     }
     
-    public void update(String msg, Location loc) {
-    	RestClient client = new RestClient("http://10.0.2.2:8888/cc1server"); //get that server
+    
+    public String update(String msg, Location loc) {
+    	//RestClient client = new RestClient("http://10.0.2.2:8888/cc1server"); //get that server
+    	RestClient client = new RestClient("http://crowd-control.appspot.com/cc1server"); //get that server
     	String ourLat = "";
     	String ourLon = "";
     	
     	//MESSAGE SENDING
     	if ((msg != null) && !msg.matches("^\\s*$")) { //we have a real message to send
-    		ENTRIES.add(Username + ": " + msg); //put the user's message on their own screen first, for a quicker feel
-        	ed.setText("");
-            lv1.setAdapter(new ArrayAdapter<String>(this, R.layout.list_item, ENTRIES)); //display the new list item
+    		
             client.AddParam("content", msg);
     	}
     	else { //no message to send
@@ -247,35 +291,39 @@ public class crowdcontrol extends Activity {
         }
  
         String response = client.getResponse();
-        if ((response != null) && !response.matches("^\\s*$")) { //this should only run when there were waiting messages on the server
+        return response;
+        /*if ((response != null) && !response.matches("^\\s*$")) { //this should only run when there were waiting messages on the server
         	response2list(response); //add any queue entries to the user's list
-        }
-
+        */
     }
     
     public void response2list(String response) {
-    	String[] respf = response.split("[~]");
-    	if (respf.length == 2) {
-    		Key = Long.valueOf(respf[0]);
-    		for (Userkey i : uk) {
-    			if (i.getUsername().equals(Username)) {
-    				i.setKey(Key);
-    				break;
-    			}
-    		}
-    		
-    		response = respf[1];
-    		Log.w("keystuff", " " + respf.length + " " + Key);
-    	}
     	if ((response != null) && !response.matches("^\\s*$")) {
-	    	String[] resp = response.split("[|]");
-	    	
-	    	Log.w("keystuff", "response: " + response + " " + resp + " " + resp.length);
-	    	for (int j = 0; j < resp.length - 1; j ++) {
-	    		String i = resp[j];
-	    		ENTRIES.add(i);
+	    	String[] respf = response.split("[~]");
+	    	if (respf.length == 2) {
+	    		Key = Long.valueOf(respf[0]);
+	    		for (Userkey i : uk) {
+	    			if (i.getUsername().equals(Username)) {
+	    				i.setKey(Key);
+	    				break;
+	    			}
+	    		}
+	    		
+	    		response = respf[1];
+	    		Log.w("keystuff", " " + respf.length + " " + Key);
 	    	}
-	    	lv1.setAdapter(new ArrayAdapter<String>(this, R.layout.list_item, ENTRIES));
+	    	if ((response != null) && !response.matches("^\\s*$")) {
+		    	String[] resp = response.split("[|]");
+		    	
+		    	Log.w("keystuff", "response: " + response + " " + resp + " " + resp.length);
+		    	for (int j = 0; j < resp.length - 1; j ++) {
+		    		String i = resp[j];
+		    		ENTRIES.add(i);
+		    	}
+		    	lv1.setAdapter(new ArrayAdapter<String>(this, R.layout.list_item, ENTRIES));
+		    	Log.w("THREADSARG", "not past here");
+	    	}
+	    	Log.w("THREADSARG", "or here either");
     	}
     }
 }
